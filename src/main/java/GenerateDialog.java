5212227800f33c5316cf853773279a26a217c597
jellyfish26 <jellyfish26@users.noreply.github.com>
2019-01-01 16:15:45 +0900

    Limited input value (close #5)

--- src/main/java/GenerateDialog.java
+++ src/main/java/GenerateDialog.java
@@ -4,7 +4,7 @@ import javafx.scene.control.TextInputDialog;
 
 class GenerateDialog {
     private String DO_NOT_QUIT_STRING = "7AE562B7CE81EA1218A088F6C4104F76D9F16027D2022DE2CE536C5727BF7C74850D6E3C6260A257A205ABC05C3126372DF807F9E73654AED7A70847E5DCA2B5";
-    int  InputNumberDialog(String header, String title) {
+    int  InputNumberDialog(String header, String title, int minValue, int maxValue) {
         TextInputDialog inputDialog = new TextInputDialog();
         inputDialog.setHeaderText(header);
         inputDialog.setTitle(title);
@@ -21,9 +21,15 @@ class GenerateDialog {
                 AnythingAlertDialog("入力してください。", "入力空です");
             } else if (!result.equals(DO_NOT_QUIT_STRING)) {
                 try {
-                    return Integer.parseInt(result);
+                    if (Integer.parseInt(result) < minValue) {
+                        AnythingAlertDialog(minValue + "以上の整数を入力してください", "入力が不正です。");
+                    } else if (Integer.parseInt(result) > maxValue) {
+                        AnythingAlertDialog(maxValue + "以下の数字を入力してください。", "入力が不正です。");
+                    } else {
+                        return Integer.parseInt(result);
+                    }
                 } catch (NumberFormatException e) {
-                    AnythingAlertDialog("数字を入力してください。", "入力が不正です。");
+                    AnythingAlertDialog("整数を入力してください。", "入力が不正です。");
                 }
             }
         }
