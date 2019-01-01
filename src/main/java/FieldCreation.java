a046424433317bc5bdf726f9be01dc102442d854
jellyfish26 <jellyfish0162@gmail.com>
2019-01-01 19:25:02 +0900

    It does not explode with the first click

--- src/main/java/FieldCreation.java
+++ src/main/java/FieldCreation.java
@@ -1,11 +1,9 @@
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.util.Random;
 
 class FieldCreation extends ClickAction {
     double rectangleLength = 50;
-    private final int BOMB = 9;
 
     void fieldInitialization(Stage stage, MineSweeper mineSweeper, int vertical, int width, int numberOfBombs) {
         usedMineSweeper = mineSweeper;
@@ -15,7 +13,10 @@ class FieldCreation extends ClickAction {
         fieldTiles = new Tile[vertical][width];
         numberOfSurroundingBombs = new int[vertical][width];
         flagInstall = new boolean[vertical][width];
-        SettingBombs(vertical, width, numberOfBombs);
+        manipulateBombs = new ManipulateBombs();
+        manipulateBombs.fieldVertical = vertical;
+        manipulateBombs.fieldWidth = width;
+        manipulateBombs.SettingBombs(numberOfBombs, -2, -2);
         stopWatch.start();
 
         for (int verticalCoordinate = 0; verticalCoordinate < vertical; ++verticalCoordinate) {
@@ -23,44 +24,14 @@ class FieldCreation extends ClickAction {
                 int surroundBomb = numberOfSurroundingBombs[verticalCoordinate][widthCoordinate];
                 System.out.print(surroundBomb);
                 if (surroundBomb == BOMB) {
-                    fieldTiles[verticalCoordinate][widthCoordinate] = new Tile("B", verticalCoordinate, widthCoordinate, this, rectangleLength);
+                    fieldTiles[verticalCoordinate][widthCoordinate] = new Tile(9, verticalCoordinate, widthCoordinate, this, rectangleLength);
                 } else {
-                    fieldTiles[verticalCoordinate][widthCoordinate] = new Tile(String.valueOf(surroundBomb), verticalCoordinate, widthCoordinate, this, rectangleLength);
+                    fieldTiles[verticalCoordinate][widthCoordinate] = new Tile( surroundBomb, verticalCoordinate, widthCoordinate, this, rectangleLength);
                 }
             }
             System.out.println();
         }
-    }
-
-    private void SettingBombs(int vertical, int width, int numberOfBombs) {
-        Random randomCoordinate = new Random();
-        for (int setting = 0; setting < numberOfBombs; ++setting) {
-            int bombVerticalCoordinate = randomCoordinate.nextInt(vertical);
-            int bombWidthCoordinate = randomCoordinate.nextInt(width);
-            if (numberOfSurroundingBombs[bombVerticalCoordinate][bombWidthCoordinate] == BOMB) {
-                --setting; // A bomb has already been installed.
-            } else {
-                numberOfSurroundingBombs[bombVerticalCoordinate][bombWidthCoordinate] = BOMB;
-                CountUpAroundBomb(bombVerticalCoordinate, bombWidthCoordinate);
-            }
-        }
-    }
-
-    private void CountUpAroundBomb(int setVerticalCoordinate, int setWidthCoordinate) {
-        /*  1 1 1
-         *  1 B 1
-         *  1 1 1 */
-        for (int vertical = -1; vertical <= 1; vertical++) {
-            for (int width = -1; width <= 1; width++) {
-                try {
-                    if (numberOfSurroundingBombs[setVerticalCoordinate + vertical][setWidthCoordinate + width] != BOMB) {
-                        ++numberOfSurroundingBombs[setVerticalCoordinate + vertical][setWidthCoordinate + width];
-                    }
-                } catch (IndexOutOfBoundsException e) {
-                    // nothing to do
-                }
-            }
-        }
+        manipulateBombs.firstClick = true;
     }
 
     void AddTileToPane(int vertical, int width, Pane pane) {
