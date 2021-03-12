f45be6913f09634c145e9e18995ce49a44b135de
jellyfish26 <jellyfish0162@gmail.com>
2021-03-12 21:02:48 +0900

    Refactoring StopWatch

--- src/main/java/StopWatch.java
+++ src/main/java/StopWatch.java
@@ -5,12 +5,30 @@ class StopWatch {
     startTime = System.currentTimeMillis();
   }
 
+  String genTimeString(String nameBase, long value) {
+    if (value == 1) {
+      return String.format(" %d %s", value, nameBase);
+    } else if(value >= 2) {
+      return String.format(" %d %ss", value, nameBase);
+    }
+    return "";
+  }
+
   String stop() {
-    String output = "";
+    StringBuilder ret = new StringBuilder();
     long elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
-    if (elapsedTime / 3600 > 0) output += (elapsedTime / 3600 + "時間");
-    if (elapsedTime / 60 > 0) output += (elapsedTime / 60 - (elapsedTime / 3600) * 60 + "分");
-    output += elapsedTime % 60 + "秒";
-    return output;
+
+    if (elapsedTime >= 3600) {
+      ret.append(genTimeString("hour", elapsedTime / 3600));
+    }
+
+    if (elapsedTime >= 60) {
+      long hours = (elapsedTime / 60) - (elapsedTime / 3600) * 60;
+      ret.append(genTimeString("minute", hours));
+    }
+
+    long seconds = elapsedTime % 60;
+    ret.append(genTimeString("second", seconds));
+    return ret.toString();
   }
 }
